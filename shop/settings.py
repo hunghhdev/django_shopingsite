252f2393965c8ac4345b024695ae8352091495f4
buzbum <hotro.giaitri23@gmail.com>
2019-11-10 23:37:34 +0700

    static file

--- shop/settings.py
+++ shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'order',
     'product',
     'user',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'user.CustomerUser'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
